921d3e62130e34485abeff044d21f16ca35abd51
jhonatanrp05 <jpachecos@uninorte.edu.co>
2026-05-18 21:57:35 -0500

    feat: add react-hook-form, zod resolver, and react-query dependencies

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,11 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toast, toastQueue } from "@heroui/react";
+
 import { ThemeProvider } from "./context/ThemeContext";
+
+const queryClient = new QueryClient();
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider>
-      {children}
-      <Toast.Provider placement="bottom end" queue={toastQueue} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        {children}
+        <Toast.Provider placement="bottom end" queue={toastQueue} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
